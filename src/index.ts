021ee8b812421be5379c6cdeba7cc20edff1ae2f
TickHeart <1243745476@qq.com>
2022-10-18 11:54:40 +0800

    complete

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export const name = 'tick'
+import path from 'path'
+import fse from 'fs-extra'
+import { execaCommandSync } from 'execa'
+function main() {
+  const isHave = fse.pathExistsSync(path.resolve(process.cwd(), '.node-version'))
+  const defaultCmd = 'fnm use default'
+  const useCmd = 'fnm use'
+  execaCommandSync(isHave ? useCmd : defaultCmd, {
+    stdio: 'inherit',
+    encoding: 'utf-8',
+  })
+}
+main()
